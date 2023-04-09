7352495df842857f3411980c4fcfeefb9c472e94
soulwax <kling.mobil@icloud.com>
2023-04-09 23:50:27 +0200

    InputHandler maintainability hotfix

--- src/main/java/com/gray17/soul/catcraft/InputHandler.java
+++ src/main/java/com/gray17/soul/catcraft/InputHandler.java
@@ -8,6 +8,7 @@ import org.bukkit.entity.Cat;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Fireball;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Ocelot;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -20,180 +21,235 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.projectiles.ProjectileSource;
+import org.bukkit.World;
 
 import com.gray17.soul.catcraft.emoji.EmojiLibrary;
 
 import static com.gray17.soul.catcraft.ConfigFile.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputHandler implements Listener {
 
-	private final PlayerHandler playerHandler;
-	private final CatCraft plugin;
-	private final Debugger debugger;
+    private final PlayerHandler playerHandler;
+    private final CatCraft plugin;
+    private final Debugger debugger;
+    private List<Class<? extends LivingEntity>> defendedEntities = new ArrayList<Class<? extends LivingEntity>>();
 
-	public InputHandler(CatCraft plugin, Debugger debugger) {
-		this.plugin = plugin;
-		this.debugger = debugger;
-		this.playerHandler = this.plugin.playerHandler;
-		this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
+    public InputHandler(CatCraft plugin, Debugger debugger) {
+        this.plugin = plugin;
+        this.debugger = debugger;
+        this.playerHandler = this.plugin.playerHandler;
+        this.plugin.getServer().getPluginManager().registerEvents(this, this.plugin);
 
-	}
+        // ? Defended entities, add more here
+        defendedEntities.add(Ocelot.class);
+        defendedEntities.add(Cat.class);
+    }
 
-	public void init() {
-	}
+    public void init() {
+    }
 
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void playerConnects(PlayerJoinEvent e) {
-		this.playerHandler.addPlayer(e.getPlayer());
-	}
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void playerConnects(PlayerJoinEvent e) {
+        this.playerHandler.addPlayer(e.getPlayer());
+    }
 
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void playerDisconnects(PlayerQuitEvent e) {
-		this.playerHandler.removePlayer(e.getPlayer());
-	}
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void playerDisconnects(PlayerQuitEvent e) {
+        this.playerHandler.removePlayer(e.getPlayer());
+    }
 
-	@EventHandler(priority = EventPriority.NORMAL)
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void catDeath(EntityDeathEvent event) {
+        LivingEntity entity = (LivingEntity) event.getEntity(); // seems redudant, but it changed since I wrote this a
+                                                                // lot
+        if (isDefendedEntity(entity)) {
+            logOcelotDeath();
 
-	public void catDeath(EntityDeathEvent e) {
-		if (e.getEntity() instanceof Ocelot || e.getEntity() instanceof Cat) {
-			if (VERBOSE) {
-				debugger.info("Ocelot died");
-			}
+            Player killer = entity.getKiller();
+            if (killer != null) {
+                punishKiller(killer);
+                notifyPlayers(killer);
+                inflictThunderstorm(entity.getWorld());
+            }
+        }
+    }
 
-			if (e.getEntity().getKiller() != null && e.getEntity().getKiller() != null) {
-				Player p = e.getEntity().getKiller();
-				p.damage(9000.0D);
-				if (VERBOSE) {
-					debugger.info(p + " received 9000 damage");
-				}
+    private void logOcelotDeath() {
+        if (VERBOSE) {
+            debugger.info("Ocelot died");
+        }
+    }
 
-				for (Player all : this.playerHandler.getPlayers()) {
-					all.sendMessage(ChatColor.DARK_RED + "" + ChatColor.BOLD + p.getName() + " killed a cat "
-							+ "and is now receiving his righteous judgement!");
-				}
+    private void punishKiller(Player killer) {
+        double damage = 9000.0D;
+        killer.damage(damage);
+        logKillerDamage(killer, damage);
+    }
 
-				e.getEntity().getWorld().setStorm(true);
-				e.getEntity().getWorld().setThundering(true);
-				e.getEntity().getWorld().setThunderDuration(1000);
-				if (VERBOSE) {
-					debugger.info("Thunderstorm inflicted");
-				}
-			}
-		}
-	}
+    private void logKillerDamage(Player killer, double damage) {
+        if (VERBOSE) {
+            debugger.info(killer + " received " + damage + " damage");
+        }
+    }
 
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void ward(EntityDamageByEntityEvent evt) {
+    private void notifyPlayers(Player killer) {
+        String message = ChatColor.DARK_RED + "" + ChatColor.BOLD + killer.getName()
+                + " killed a cat and is now receiving his righteous judgement!";
+        for (Player player : playerHandler.getPlayers()) {
+            player.sendMessage(message);
+        }
+    }
 
-		double dmg = evt.getDamage();
-		Entity damaged = evt.getEntity();
-		Entity damager = evt.getDamager() instanceof Arrow ? (Entity) ((Arrow) evt.getDamager()).getShooter()
-				: evt.getDamager();
-		if (VERBOSE) {
-			// if verbose mode is only for player vs player interaction AND (thus); if the
-			// damager is not a player AND the damaged not one, do nothing
-			if (!VERBOSE_PLAYER_ONLY || (damaged instanceof Player && damager instanceof Player))
-				if (evt.getDamager() instanceof Arrow) {
-					ProjectileSource attacker = ((Arrow) evt.getDamager()).getShooter();
-					debugger.info(evt.getEntity() + " was harmed by " + evt.getDamager() + " of the type "
-							+ evt.getDamager().getType() + ", shooter: " + attacker + ", damage: " + dmg);
-				} else if (evt.getDamager() instanceof Fireball) {
-					ProjectileSource attacker = ((Fireball) evt.getDamager()).getShooter();
-					debugger.info(evt.getEntity() + " was harmed by " + evt.getDamager() + " of the type "
-							+ evt.getDamager().getType() + ", shooter: " + attacker + ", damage: " + dmg);
-				} else {
-					debugger.info(evt.getEntity() + " was harmed by " + evt.getDamager() + " of the type "
-							+ evt.getDamager().getType() + ", damage: " + dmg);
-				}
-		}
+    private void inflictThunderstorm(World world) {
+        world.setStorm(true);
+        world.setThundering(true);
+        world.setThunderDuration(1000);
+        logThunderstormInflicted();
+    }
 
-		if (evt.getEntity() instanceof Ocelot || evt.getEntity() instanceof Cat) {
-			if (VERBOSE) {
-				this.plugin.getLogger().info("A defended entity has been attacked! Type: " + evt.getEntity());
-			}
+    private void logThunderstormInflicted() {
+        if (VERBOSE) {
+            debugger.info("Thunderstorm inflicted");
+        }
+    }
 
-			Player offender;
-			Entity ddealer = evt.getDamager();
-			if (ddealer instanceof Player) {
-				if (VERBOSE) {
-					debugger.info("Damager :" + ddealer + " is a Player");
-				}
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void ward(EntityDamageByEntityEvent evt) {
+        double damage = evt.getDamage();
+        Entity damaged = evt.getEntity();
+        Entity damager = getDamagerFromEvent(evt);
 
-				offender = (Player) ddealer;
-				offender.damage(dmg);
-				if (VERBOSE) {
-					debugger.info(ddealer + " received " + dmg + " damage");
-				}
-			} else if (ddealer instanceof Arrow) {
-				if (VERBOSE) {
-					debugger.info("Damager was an arrow, owner: " + ((Arrow) ddealer).getShooter());
-				}
+        logDamageEvent(damaged, damager, damage);
 
-				offender = (Player) ((Arrow) ddealer).getShooter();
-				if(offender != null) {
-					offender.damage(dmg);
-					if (VERBOSE) {
-						debugger.info(((Arrow) ddealer).getShooter() + " received " + dmg + " damage.");
-					}
-				}
-			}
-		}
-	}
+        if (isDefendedEntity(damaged)) {
+            Player offender = getOffenderFromDamager(damager);
+            if (offender != null) {
+                offender.damage(damage);
+                logOffenderDamage(offender, damage);
+            }
+        }
+    }
 
-	@EventHandler(priority = EventPriority.LOW)
-	public void playerInteract(InventoryOpenEvent evt) {
-		if(!SHOULD_RELAY_CHESTS && !VERBOSE) return;
+    private Entity getDamagerFromEvent(EntityDamageByEntityEvent evt) {
+        if (evt.getDamager() instanceof Arrow) {
+            return (Entity) ((Arrow) evt.getDamager()).getShooter();
+        }
+        return evt.getDamager();
+    }
 
-		HumanEntity player = evt.getPlayer();
-		Location loc = player.getLocation();
-		evt.getInventory().getType();
-		debugger.info("Player " + player.getName() + " opened " + evt.getInventory().getType()
-				+ " at location: x=" + loc.getBlockX() + " y=" + loc.getBlockY() + " z=" + loc.getBlockZ());
-	}
+    private void logDamageEvent(Entity damaged, Entity damager, double damage) {
+        if (!VERBOSE) {
+            return;
+        }
 
-	@EventHandler(priority = EventPriority.HIGHEST)
-	public void playerSentMessage(AsyncPlayerChatEvent event) {
-		Player p = event.getPlayer();
-		String message = event.getMessage();
-		String messageModified = EmojiLibrary.findAndReplaceEmojiRND(message);
+        if (shouldLogDamageEvent(damaged, damager)) {
+            String message = buildDamageMessage(damaged, damager, damage);
+            debugger.info(message);
+        }
+    }
 
-		if (messageModified.isEmpty()) {
-			messageModified = message;
-		}
+    private boolean shouldLogDamageEvent(Entity damaged, Entity damager) {
+        return !VERBOSE_PLAYER_ONLY || (damaged instanceof Player && damager instanceof Player);
+    }
 
-		if (event.isAsynchronous()) {
-			String formatResult = InputHandler.setFormat(p, messageModified);
-			event.setFormat(formatResult);
-			event.setMessage(messageModified);
-		}
-	}
+    private String buildDamageMessage(Entity damaged, Entity damager, double damage) {
+        String message = damaged + " was harmed by " + damager + " of the type " + damager.getType() + ", damage: "
+                + damage;
 
-	public static String setFormat (CommandSender sender, String message) {
-		if(sender instanceof Player p) {
-			String result;
-			if (p.hasPermission("chat.format.member")) {
-				result = ChatColor.DARK_GRAY + "[" + ChatColor.DARK_GREEN + "MEMBER" + ChatColor.DARK_GRAY + "] "
-						+ ChatColor.DARK_GREEN + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE
-						+ message;
-				// This will give the player with that permission node that Chat format.
-			} else if (p.hasPermission("chat.format.moderator")) {
-				result = ChatColor.DARK_GRAY + "[" + ChatColor.RED + "MODERATOR" + ChatColor.DARK_GRAY + "] " + ChatColor.RED
-						+ p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message;
-				// This will give the player with that permission node that Chat format.
-			} else if (p.hasPermission("chat.format.admin")) {
-				result = ChatColor.DARK_GRAY + "[" + ChatColor.DARK_RED + "ADMIN" + ChatColor.DARK_GRAY + "] "
-						+ ChatColor.DARK_RED + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE
-						+ message;
-				// This will give the player with that permission node that Chat format.
-			} else {
-				result = ChatColor.DARK_GRAY + "[" + ChatColor.DARK_GREEN + "MEMBER" + ChatColor.DARK_GRAY + "] "
-						+ ChatColor.DARK_GREEN + p.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE
-						+ message;
-			}
-			return result;
-		} else {
-			return ChatColor.DARK_GRAY + "[" + ChatColor.MAGIC + "SERVER" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE
-					+ "CONSOLE" + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message;
-		}
-	}
+        if (damager instanceof Arrow || damager instanceof Fireball) {
+            ProjectileSource attacker = damager instanceof Arrow ? ((Arrow) damager).getShooter()
+                    : ((Fireball) damager).getShooter();
+            message += ", shooter: " + attacker;
+        }
+
+        return message;
+    }
+
+    private boolean isDefendedEntity(Entity entity) {
+        for (Class<? extends LivingEntity> defendedEntity : defendedEntities) {
+            if (defendedEntity.isInstance(entity)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Player getOffenderFromDamager(Entity damager) {
+        if (damager instanceof Player) {
+            return (Player) damager;
+        } else if (damager instanceof Arrow) {
+            return (Player) ((Arrow) damager).getShooter();
+        }
+        return null;
+    }
+
+    private void logOffenderDamage(Player offender, double damage) {
+        if (VERBOSE) {
+            debugger.info(offender + " received " + damage + " damage");
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void playerInteract(InventoryOpenEvent evt) {
+        if (!SHOULD_RELAY_CHESTS && !VERBOSE)
+            return;
+
+        HumanEntity player = evt.getPlayer();
+        Location loc = player.getLocation();
+        evt.getInventory().getType();
+        debugger.info("Player " + player.getName() + " opened " + evt.getInventory().getType()
+                + " at location: x=" + loc.getBlockX() + " y=" + loc.getBlockY() + " z=" + loc.getBlockZ());
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void playerSentMessage(AsyncPlayerChatEvent event) {
+        Player p = event.getPlayer();
+        String message = event.getMessage();
+        String messageModified = EmojiLibrary.findAndReplaceEmojiRND(message);
+
+        if (messageModified.isEmpty()) {
+            messageModified = message;
+        }
+
+        if (event.isAsynchronous()) {
+            String formatResult = InputHandler.setFormat(p, messageModified);
+            event.setFormat(formatResult);
+            event.setMessage(messageModified);
+        }
+    }
+
+    public static String setFormat(CommandSender sender, String message) {
+        if (sender instanceof Player player) {
+            return formatPlayerMessage(player, message);
+        } else {
+            return formatConsoleMessage(message);
+        }
+    }
+
+    private static String formatPlayerMessage(Player player, String message) {
+        ChatColor roleColor;
+        String role;
+
+        if (player.hasPermission("chat.format.admin")) {
+            roleColor = ChatColor.DARK_RED;
+            role = "ADMIN";
+        } else if (player.hasPermission("chat.format.moderator")) {
+            roleColor = ChatColor.RED;
+            role = "MODERATOR";
+        } else {
+            roleColor = ChatColor.DARK_GREEN;
+            role = "MEMBER";
+        }
+
+        return ChatColor.DARK_GRAY + "[" + roleColor + role + ChatColor.DARK_GRAY + "] "
+                + roleColor + player.getDisplayName() + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message;
+    }
+
+    private static String formatConsoleMessage(String message) {
+        return ChatColor.DARK_GRAY + "[" + ChatColor.MAGIC + "SERVER" + ChatColor.DARK_GRAY + "] " + ChatColor.WHITE
+                + "CONSOLE" + ChatColor.DARK_GRAY + ": " + ChatColor.WHITE + message;
+    }
 }
